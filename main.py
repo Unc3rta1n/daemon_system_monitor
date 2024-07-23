309778f291f8619ca75d133cce5e88083fe78479
Nikita Yamatin <yamatin_nikita@cybersec74.ru>
2024-07-23 17:28:40 +0500

    123 initial

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-import this
+import subprocess
+
+# Step 1: Run the command and capture the output
+result = subprocess.run(['df', '-i'], capture_output=True, text=True)
+
+print(result.stdout)
+# вот так и будем кетчить, а потом парсить строку))
